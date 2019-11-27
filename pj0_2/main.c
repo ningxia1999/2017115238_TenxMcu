af69ee49e966c9811511ca5360bdc6c87c325601
2017115238TenxMcu <2029552001@qq.com>
2019-11-27 21:59:27 +0800

    使用 T2 1ms 定时驱动程序运行的架构

--- pj0_2/main.c
+++ pj0_2/main.c
@@ -1,25 +1,39 @@
 void main() {
   SysInit();
   VarsInit();
-  F_ledOn();
-  DelayMs(500);
+
   //使能看门狗
   F_turnOnWDT();
 
   while (1) {
     //喂狗
       F_clearWDT();
-   // F_ledOff();
-    // delayMs(500);
-    // F_ledOn();
-    // delayMs(500);
-  
-    GetKeys();
-    if (D_keyValue1 == keyValue) {
-      F_ledNeg();
-    }
-    keyValue = D_keyNull;
+   //业务代码
+    TimeProcess();
+    TaskSetting();
+    TaskProcess();
+    DisplayProcess();
   }
+}
+//=============================================================================
+void TimeProcess() {
+  static uint8_t timer5ms = 0;
+  static uint16_t second = 0;
+
+  if (b1ms) {
+    // 1ms 执行一次
+    b1ms = 0;
+    timer5ms++;
+    second++;
+  }
+  if (timer5ms >= D_5ms) {
+
+    GetKeys();
+     }
+  if (second >= D_1000ms) {
+    // 1s 执行一次
+    second = 0;
+   }
 }
 //=============================================================================
 void DelayMs(uint16_t msCount) {
