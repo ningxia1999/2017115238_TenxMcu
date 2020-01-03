ce7673ea65139a5d0a2d9693d980c568ae3e2599
2017115238TenxMcu <2029552001@qq.com>
2020-01-03 20:24:28 +0800

    no message

---
+++ project 1/led灯点亮/init.h
@@ -0,0 +1,17 @@
+#ifndef __init_h
+#define __init_h
+// Hal: exp: #define P_led P11 -----------------
+
+// Const: exp: #define D_data 1 ----------------
+
+// Globle Var -----------------------------------------
+#ifdef __init_c
+
+#else
+
+#endif
+
+void SysInit();
+void VarsInit();
+
+#endif
